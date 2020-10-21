7d30317ac22aec0441b954c0d040fcac3059f893
Aaron <aaron.pillert@ark.org>
2020-10-21 16:56:01 -0500

    Added initial redux logic and shopping cart logic

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,27 @@
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODCUT_LIST_FAILED, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODCUT_DETAILS_FAILED } from '../constants/productConstants';
+
+export const productListReducer = (state = { products: [] }, action) => {
+    switch (action.type) {
+        case PRODUCT_LIST_REQUEST:
+            return { loading: true, products: {} }
+        case PRODUCT_LIST_SUCCESS:
+            return { loading: false, products: action.payload }
+        case PRODCUT_LIST_FAILED:
+            return { loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
+
+export const productDetailsReducer = (state = { product: { reviews: [] } }, action) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return { loading: true, ...state }
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload }
+        case PRODCUT_DETAILS_FAILED:
+            return { loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
