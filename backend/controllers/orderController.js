1fd6a92317d58ad73a41d28955ccac0d9b90dfab
Sir Whiskas <atpillert@gmail.com>
2020-10-30 22:17:20 -0500

    Created pay order endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -12,13 +12,13 @@ const addOrderItems = asyncHandler(async (req, res) => {
         throw new Error('No order items')
     } else {
         const order = new Order({
-            orderItems, 
+            orderItems,
             user: req.user._id,
-            shippingAddress, 
-            paymentMethod, 
-            itemsPrice, 
-            taxPrice, 
-            shippingPrice, 
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
             totalPrice
         })
 
@@ -32,7 +32,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id).populate('user', 'name, email')
+    const order = await Order.findById(req.params.id).populate('user', 'name email')
 
     if (order) {
         res.json(order)
@@ -42,7 +42,33 @@ const getOrderById = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if (order) {
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        }
+
+        const updatedOrder = await order.save()
+
+        res.json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
 export {
     addOrderItems,
-    getOrderById
+    getOrderById,
+    updateOrderToPaid
 }
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,7 +1,8 @@
-import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL } from '../constants/orderConstants'
+import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL, ORDER_PAY_RESET, ORDER_PAY_SUCCESS } from '../constants/orderConstants'
+import { ORDER_PAY_REQUEST, ORDER_PAY_FAIL } from './../constants/orderConstants';
 
 export const orderCreateReducer = (state = {}, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case ORDER_CREATE_REQUEST:
             return {
                 loading: true
@@ -23,7 +24,7 @@ export const orderCreateReducer = (state = {}, action) => {
 }
 
 export const orderDetailsReducer = (state = { loading: true, orderItems: [], shippingAddress: {} }, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case ORDER_DETAILS_REQUEST:
             return {
                 ...state,
@@ -39,6 +40,29 @@ export const orderDetailsReducer = (state = { loading: true, orderItems: [], shi
                 loading: false,
                 error: action.payload
             }
+        default:
+            return state
+    }
+}
+
+export const orderPayReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ORDER_PAY_REQUEST:
+            return {
+                loading: true
+            }
+        case ORDER_PAY_SUCCESS:
+            return {
+                loading: false,
+                success: true
+            }
+        case ORDER_PAY_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+        case ORDER_PAY_RESET:
+            return {}
         default:
             return state
     }
